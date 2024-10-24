791b2465733de10905b2619ede0e02be0ae87a8d
Alzhar-Crown <alzhar237631@student.smkn1kandeman.sch.id>
2024-10-24 14:58:01 +0700

    doneee

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 function main() {
-  
+    let hasilDadu = [1, 5, 2, 6, 2, 3]
+    let kemunculanGanjil = 0
+    let kemunculanGenap = 0
+
+    for (let i = 0; i < 6; i++) {
+        if (hasilDadu[i] % 2 == 0) {
+            kemunculanGenap++
+        }
+        else if (hasilDadu[i] % 2 != 0) {
+            kemunculanGanjil++
+        }
 
 
+    }
+    console.log(`Raka mendapatkan angka ganjil sebanyak ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap} kali`)
 }
+main()
+
+
+
 
 module.exports = main
